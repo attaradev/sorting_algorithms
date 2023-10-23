2df0d058e90edc6946cd3f0ac951c4d11170396f
Mike Attara <mpyebattara@gmail.com>
2023-10-23 23:16:48 +0000

    refactor

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,25 @@
-#include <stdio.h>
-
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    int swapped;
 
-    if (size <= 1)
-        return;
+	size_t i, index, tmp = 0;
 
-    for (size_t i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (size_t j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                // Swap array[j] and array[j + 1]
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-
-                // Print the array after each swap
-                for (size_t k = 0; k < size; k++)
-                {
-                    printf("%d", array[k]);
-                    if (k < size - 1)
-                        printf(", ");
-                }
-                printf("\n");
-            }
-        }
-
-        // If no swaps occurred in this pass, the array is already sorted.
-        if (!swapped)
-            break;
-    }
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
+			}
+		}
 }
