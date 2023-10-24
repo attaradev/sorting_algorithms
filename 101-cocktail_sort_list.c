00b58fe7c86677b2e8c5311836065f4c2f835d6e
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-10-24 02:38:13 +0000

    Update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -5,7 +5,7 @@
  *@node: node
  *@list: node list
  *Return: void
-*/
+ */
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
 	if (left->prev)
@@ -23,7 +23,7 @@ void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
  *cocktail_sort_list - this is a cocktail-sort implementation
  *working on doubly linked lists
  *@list: list
-*/
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
@@ -50,6 +50,7 @@ void cocktail_sort_list(listint_t **list)
 		}
 	        if (!swapped)
 	            	break;
+
 	        swapped = 0;
 	        end = curr;
 	        while (curr->prev != start) {
