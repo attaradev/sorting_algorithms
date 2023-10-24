e798510e852eee948deb571e28c8d91fbe5f85d9
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-10-24 02:18:58 +0000

    Update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -5,56 +5,58 @@
  *@node: node
  *@list: node list
  *Return: void
- */
+*/
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right) {
 	if (left->prev)
-        	left->prev->next = right;
+		left->prev->next = right;
 	if (right->next)
-        	right->next->prev = left;
-    	left->next = right->next;
+		right->next->prev = left;
+	left->next = right->next;
 	right->prev = left->prev;
 	left->prev = right;
 	right->next = left;
 	if (*list == left)
-        	*list = right;
+		*list = right;
 }
 /**
  *cocktail_sort_list - this is a cocktail-sort implementation
  *working on doubly linked lists
  *@list: list
- */
+*/
 void cocktail_sort_list(listint_t **list) {
 	int swapped;
-	
+
 	listint_t *start = NULL;
 	listint_t *end = NULL;
 	listint_t *curr = NULL;
 	if (!list || !(*list) || !(*list)->next)
-        	return;
+		return;
 	do {
-	        swapped = 0;
-	        curr = start = *list;
-	        while (curr->next != end) {
-	        	if (curr->n > curr->next->n) {
-		                swap_nodes(list, curr, curr->next);
-		                swapped = 1;
-		                print_list(*list);
-	            	}
-	            	else
-	                	curr = curr->next;
-	        }
+		swapped = 0;
+		curr = start = *list;
+		while (curr->next != end) 
+		{
+			if (curr->n > curr->next->n)
+			{
+				swap_nodes(list, curr, curr->next);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				curr = curr->next;
+		}
 	        if (!swapped)
 	            	break;
 	        swapped = 0;
 	        end = curr;
 	        while (curr->prev != start) {
-	            	if (curr->n < curr->prev->n) {
-		                swap_nodes(list, curr->prev, curr);
-		                swapped = 1;
-		                print_list(*list);
+			if (curr->n < curr->prev->n) {
+				swap_nodes(list, curr->prev, curr);
+				swapped = 1;
+				print_list(*list);
 			}
-		    	else
+			else
 				curr = curr->prev;
-	        }
-    	} while (swapped);
+		}
+	} while (swapped);
 }
