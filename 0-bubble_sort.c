3c967821440c3f114bc254d9e88b5eec99632cfc
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-10-24 01:40:41 +0000

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@ void bubble_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		
+
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -28,7 +28,7 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-		
+
 		if (!swapped)
 			break;
 	}
