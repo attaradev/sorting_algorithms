c542d526572ebc813e26765441899b4f2db6475d
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-10-24 01:20:12 +0000

    refactor

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,15 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	int swapped, temp = 0;
+	int temp;
+	int swapped;
 
-	if (size <= 1)
+	if (size < 2)
 		return;
-	
 	for (size_t i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		for (size_t j = 0; j < size - 1; j++)
+		for (size_t j = 0; index < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -23,13 +23,11 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
-
 				print_array(array, size);
 			}
 		}
-
+		
 		if (!swapped)
 			break;
 	}
-			
 }
