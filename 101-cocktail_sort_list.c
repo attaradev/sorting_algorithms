0ea10199c45bff3fe2dbebd053aa1216b6e0abc3
Mike Attara <31483629+mikeattara@users.noreply.github.com>
2023-10-24 02:11:18 +0000

    Update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,19 +7,17 @@
  *Return: void
  */
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right) {
-    if (left->prev)
-        left->prev->next = right;
-    if (right->next)
-        right->next->prev = left;
-    left->next = right->next;
-    right->prev = left->prev;
-    left->prev = right;
-    right->next = left;
-
-    if (*list == left)
-        *list = right;
+	if (left->prev)
+        	left->prev->next = right;
+	if (right->next)
+        	right->next->prev = left;
+    	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
+	if (*list == left)
+        	*list = right;
 }
-
 /**
  *cocktail_sort_list - this is a cocktail-sort implementation
  *working on doubly linked lists
@@ -27,17 +25,15 @@ void swap_nodes(listint_t **list, listint_t *left, listint_t *right) {
  */
 void cocktail_sort_list(listint_t **list) {
 	int swapped;
+	
 	listint_t *start = NULL;
 	listint_t *end = NULL;
 	listint_t *curr = NULL;
-
 	if (!list || !(*list) || !(*list)->next)
         	return;
-
 	do {
 	        swapped = 0;
 	        curr = start = *list;
-	
 	        while (curr->next != end) {
 	        	if (curr->n > curr->next->n) {
 		                swap_nodes(list, curr, curr->next);
@@ -47,13 +43,10 @@ void cocktail_sort_list(listint_t **list) {
 	            	else
 	                	curr = curr->next;
 	        }
-
 	        if (!swapped)
 	            	break;
-	
 	        swapped = 0;
 	        end = curr;
-	
 	        while (curr->prev != start) {
 	            	if (curr->n < curr->prev->n) {
 		                swap_nodes(list, curr->prev, curr);
